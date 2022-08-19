b8ec41ac0182257bff3977e73ca6212f00929169
Andy Watson <werdnamac@gmail.com>
2022-08-19 19:26:30 -0400

    Migrated the business object to App.js and added a businesses array. Modified the App.js, BusinessList.js and Business components to work now that the business object has moved from Business.js to App.js and the list of businesses has moved from BusinessList to App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [business, business, business, business, business, business];
+
 class App extends React.Component {
 
   render() {
   return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Ravenous</h1>
         <SearchBar />
-        <BusinessList /> 
+        <BusinessList businesses={businesses}/> 
       </div>
   )};
 }
